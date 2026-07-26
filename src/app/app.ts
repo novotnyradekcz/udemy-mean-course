e904ead68da7e21216038a558765c01bf1d38435
Radek Novotny <rano@ciklum.com>
2026-07-26 08:49:40 -0300

    Added dummy PostCreate component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PostCreateComponent } from './posts/post-create/post-create.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [
+    RouterOutlet,
+    PostCreateComponent
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
