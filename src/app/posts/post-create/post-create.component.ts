a64b4e11d422620f1227408da14a4d3ee453e6e1
Radek Novotny <rano@ciklum.com>
2026-07-26 09:12:25 -0300

    Added reading user input

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-post-create',
+  standalone: true,
+  imports: [
+    FormsModule
+  ],
   templateUrl: './post-create.component.html'
 })
 export class PostCreateComponent {
+  enteredValue = '';
+  newPost = 'NO CONTENT';
+
   onAddPost() {
-    alert('Post added!');
+    this.newPost = this.enteredValue;
   }
 }
