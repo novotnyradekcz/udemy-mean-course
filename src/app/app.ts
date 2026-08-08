2d3357be51b064bb77fde6b7f03258e733f3bda6
Radek Novotny <rano@ciklum.com>
2026-08-08 20:44:07 -0300

    Added a header

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
+    HeaderComponent,
     RouterOutlet,
     PostCreateComponent
   ],
