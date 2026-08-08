eff8594a922cd31556c7845a0f05370d077ab321
Radek Novotny <rano@ciklum.com>
2026-08-08 20:34:04 -0300

    Added Angular Material

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-post-create',
   standalone: true,
   imports: [
-    FormsModule
+    FormsModule,
+    MatButtonModule,
+    MatCardModule,
+    MatInputModule
   ],
-  templateUrl: './post-create.component.html'
+  templateUrl: './post-create.component.html',
+  styleUrl: './post-create.component.css'
 })
 export class PostCreateComponent {
   enteredValue = '';
