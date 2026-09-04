122d80eef0c55b14305d5e40d7216603da767a72
Radek Novotny <novotnyradekcz@gmail.com>
2026-09-03 22:33:03 -0300

    Fetching posts from server

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
 
@@ -8,8 +9,14 @@ export class PostsService {
   private posts: Post[] = [];
   private postsUpdated = new Subject<Post[]>();
 
+  constructor(private http: HttpClient) {}
+
   getPosts() {
-    return [...this.posts];
+    this.http.get<{message: string, posts: Post[]}>('http://localhost:3000/api/posts')
+      .subscribe((postData) => {
+        this.posts = postData.posts;
+        this.postsUpdated.next([...this.posts]);
+      });
   }
 
   getPostUpdateListener() {
@@ -18,8 +25,9 @@ export class PostsService {
 
   addPost(title: string, content: string) {
     const post: Post = {
-      title,
-      content
+      id: null,
+      title: title,
+      content: content
     };
     this.posts.push(post);
     this.postsUpdated.next([...this.posts]);
